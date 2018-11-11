661920e558b84a6acd68367e953a361c08b51a15
Sergii <sergiigrytsaienko@gmail.com>
2018-11-11 13:33:22 -0600

    routes&componnents added

---
+++ resources/assets/js/components/ChatApp.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="chat-app">
+        <Conversation
+                :contact="selectedContact"
+                :messages="messages"
+        />
+        <ContactsList
+                :contacts="contacts"
+        />
+    </div>
+</template>
+
+<script>
+    import Conversation from './Conversation';
+    import ContactsList from './ContactsList';
+
+    export default {
+        components: {Conversation, ContactsList},
+        props: {
+            user: {
+                type: Object,
+                required: true
+            }
+        },
+        data() {
+            return {
+                selectedContact: null,
+                messages: [],
+                contacts: []
+            }
+        },
+        mounted() {
+            console.log(this.user)
+            axios.get('/contacts')
+                .then(response => {
+                    console.log(response.data);
+                    this.contacts = response.data;
+                })
+        }
+    }
+</script>
---
+++ resources/assets/js/components/ContactsList.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="contact-list">
+        <ul>
+            <li v-for="contact in contacts"
+                :key="contact.id"
+            >
+                <div class="avatar">
+                    <img :src="contact.profile_image" :alt="contact.name">
+                </div>
+                <div class="contact">
+                    <div class="name">{{contact.name}}</div>
+                    <div class="email">{{contact.email}}</div>
+                </div>
+
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "ContactsList",
+        props: {
+            contacts: {
+                type: Array,
+                default: []
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
